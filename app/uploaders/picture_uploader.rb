9404dca02e3701307544b555d678fcd34daff986
Tom Ka Chun Cheung <tomhk1123@gmail.com>
2016-11-30 16:28:59 -0500

    Add production image upload

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -7,9 +7,12 @@ class PictureUploader < CarrierWave::Uploader::Base
   # resizing all images to [w, l] regardless of size
   process resize_to_limit: [300, 300]
 
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
-  storage :file
-  # storage :fog
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
